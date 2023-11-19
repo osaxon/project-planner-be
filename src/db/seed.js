347eebe74de2a04029ba8313e8f18d49bfd2f847
Oliver Saxon <oliverrsaxon@gmail.com>
2023-11-19 18:09:16 +0000

    refactors seed to use async await

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,42 +1,50 @@
 const db = require("./");
 
-const seed = ({ projects, tasks, categories, users }) => {
-    return Promise.all([
-        dropTable("tasks"),
-        dropTable("categories"),
-        dropTable("users"),
-        dropTable("projects"),
-    ])
-        .then(() => {
-            return createProjectsTable();
-        })
-        .then(() => {
-            return createUsersTable();
-        })
-        .then(() => {
-            console.log("tables created 😎");
-        })
-        .catch((error) => console.log(error));
+const seed = async ({ projects, tasks, categories, users }) => {
+    try {
+        await dropTable("tasks");
+        await dropTable("categories");
+        await dropTable("users");
+        await dropTable("projects");
+
+        await createUsersTable();
+        await createProjectsTable();
+        console.log("Tables created 😎");
+    } catch (error) {
+        throw error;
+    }
 };
 
-function dropTable(tableName) {
-    return db.query(`DROP TABLE IF EXISTS ${tableName}`);
+async function dropTable(tableName) {
+    try {
+        await db.query(`DROP TABLE IF EXISTS ${tableName}`);
+    } catch (error) {
+        throw error;
+    }
 }
 
-function createProjectsTable() {
-    return db.query(`CREATE TABLE projects (
+async function createProjectsTable() {
+    try {
+        await db.query(`CREATE TABLE projects (
                 project_id SERIAL PRIMARY KEY,
                 project_name VARCHAR(50) NOT NULL,
                 start_date DATE NOT NULL
             );`);
+    } catch (error) {
+        throw error;
+    }
 }
 
-function createUsersTable() {
-    return db.query(`CREATE TABLE users (
+async function createUsersTable() {
+    try {
+        db.query(`CREATE TABLE users (
         user_id SERIAL PRIMARY KEY,
         username VARCHAR(50) NOT NULL,
         email VARCHAR(100) NOT NULL
     )`);
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = seed;
